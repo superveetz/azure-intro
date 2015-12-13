db33a872399177c9c95423c6eda548345e5d0485
Superveetz <alexdiv87@hotmail.com>
2015-12-12 19:25:30 -0800

    fixed server port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 // define routes
 app.use(require('./routes/todos.js'));
 
-var port = process.node.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
   console.log('...on ' + port);
